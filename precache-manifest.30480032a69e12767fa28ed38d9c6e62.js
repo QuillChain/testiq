f922653bf6c6179239858d622afed9746eb14ac5
quillchain <hoaanhpoker@gmail.com>
2022-03-11 12:39:55 +0700

    Updates

---
+++ precache-manifest.30480032a69e12767fa28ed38d9c6e62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b489371fd8721bf0a526",
+    "url": "/testiq/static/css/main.86b22fb0.chunk.css"
+  },
+  {
+    "revision": "b489371fd8721bf0a526",
+    "url": "/testiq/static/js/main.ab883d1f.chunk.js"
+  },
+  {
+    "revision": "f1f72ec8a89df978601e",
+    "url": "/testiq/static/js/runtime~main.9bf1cfed.js"
+  },
+  {
+    "revision": "84fbb41c8e7a4f381b2f",
+    "url": "/testiq/static/js/2.d8dd5657.chunk.js"
+  },
+  {
+    "revision": "0b1bc702274bbba6eab3f864ed511135",
+    "url": "/testiq/index.html"
+  }
+];
